078332f5772d2feeb59a91b7c556939bb9da6bb0
Kameron Close <44491221+kamclose@users.noreply.github.com>
2023-07-30 09:22:33 +0500

    Update Signup.js

--- react/src/Components/Signup.js
+++ react/src/Components/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, Navigate } from "react-router-dom";
-
 import "./style1.css";
 
 export default function Signup() {
